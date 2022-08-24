3a316eb74aa06edbeb9ccd7cb1bb7526042e9d49
Tijil Agrawal <77985046+Tijil2111@users.noreply.github.com>
2022-08-24 21:25:02 +0530

    Previous Month Stats Done

---
+++ pages/previous.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "../styles/previous.module.css";
+import Head from "next/head";
+import PieChart from "../components/Dashboard/PieChart";
+
+export default function Previous() {
+  return (
+    <div>
+      <Head>
+        <title>Dashboard - Lectricus</title>
+      </Head>
+      <div className={styles.heading}>
+        Previous <span className={styles.sub}>Month</span> Data
+      </div>
+      <div className={styles.sub2}>JULY</div>
+      <div className={styles.chartDiv}>
+        <PieChart unit1={100} unit2={30} unit3={40} unit4={110} />
+      </div>
+      <div className={styles.sub2}>JUNE</div>
+      <div className={styles.chartDiv}>
+        <PieChart unit1={210} unit2={20} unit3={110} unit4={190} />
+      </div>
+      <div className={styles.sub2}>MAY</div>
+      <div className={styles.chartDiv}>
+        <PieChart unit1={130} unit2={40} unit3={190} unit4={30} />
+      </div>
+      <div className={styles.sub3}>No more data before this...</div>
+    </div>
+  );
+}
