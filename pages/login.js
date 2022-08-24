c3413a094d2040c7146d4825ee99c7cd2004931c
Sm09 <shauryameh09@gmail.com>
2022-08-24 17:34:34 +0530

    login

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,39 @@
 import styles from "../styles/auth.module.css";
 import { useState } from "react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const submit = async () => {
+    const bodyData = {
+      email,
+      password,
+    };
+    setLoading(true);
+    const res = await fetch("https://xquisite-backend.herokuapp.com/api/auth", {
+      body: JSON.stringify(bodyData),
+      method: "POST",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    if (data.success) {
+      localStorage.setItem("token", data.token);
+      router.push("/add");
+    } else {
+      alert(data.msg);
+    }
+    setLoading(false);
+  };
 
   return (
     <>
@@ -36,7 +65,9 @@ export default function Login() {
               id={styles.lastInput}
             />
           </div>
-          <button id={styles.submit}>Login!</button>
+          <button id={styles.submit} onClick={submit}>
+            Login!
+          </button>
         </div>
       </div>
     </>
